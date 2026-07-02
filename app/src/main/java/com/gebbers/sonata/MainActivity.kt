f738e50c9a76e40fa262b83d0f3b4bb71777cb14
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-02 16:29:22 +0800

    feat: create stub main activity

---
+++ app/src/main/java/com/gebbers/sonata/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gebbers.sonata
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Basic setup for now
+    }
+}
