78ca595e207461afca52cef8767fffb06cff6e17
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-06 16:38:41 +0800

    feat: create database module with Room providers

---
+++ app/src/main/java/com/gebbers/sonata/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.gebbers.sonata.di
+
+import android.content.Context
+import androidx.room.Room
+import com.gebbers.sonata.data.local.SongDao
+import com.gebbers.sonata.data.local.SonataDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): SonataDatabase {
+        return Room.databaseBuilder(
+            context,
+            SonataDatabase::class.java,
+            SonataDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSongDao(database: SonataDatabase): SongDao {
+        return database.songDao()
+    }
+}
