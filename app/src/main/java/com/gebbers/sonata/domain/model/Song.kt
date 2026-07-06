7dd50bafa9869729a9437fcf713aed52c6e5a8ea
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-06 16:39:35 +0800

    feat: create song domain model

---
+++ app/src/main/java/com/gebbers/sonata/domain/model/Song.kt
@@ -0,0 +1,12 @@
+package com.gebbers.sonata.domain.model
+
+data class Song(
+    val mediaStoreId: Long,
+    val title: String,
+    val artist: String,
+    val album: String,
+    val duration: Long,
+    val dataPath: String,
+    val uri: String,
+    val albumId: Long
+)
