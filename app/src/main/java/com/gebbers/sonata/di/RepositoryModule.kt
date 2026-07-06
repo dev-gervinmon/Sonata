935f980b569c11ba64602adce8950c53127e35f1
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-06 16:41:08 +0800

    feat: create repository module and bind MusicRepository

---
+++ app/src/main/java/com/gebbers/sonata/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.gebbers.sonata.di
+
+import com.gebbers.sonata.data.repository.MusicRepositoryImpl
+import com.gebbers.sonata.domain.repository.MusicRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindMusicRepository(
+        musicRepositoryImpl: MusicRepositoryImpl
+    ): MusicRepository
+}
