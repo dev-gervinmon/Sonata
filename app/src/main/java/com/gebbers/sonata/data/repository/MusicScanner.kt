e7fcccee43504e53b00b0637e0818d479de83ff7
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-06 16:40:11 +0800

    feat: create MusicScanner to fetch songs from MediaStore

---
+++ app/src/main/java/com/gebbers/sonata/data/repository/MusicScanner.kt
@@ -0,0 +1,76 @@
+package com.gebbers.sonata.data.repository
+
+import android.content.ContentUris
+import android.content.Context
+import android.provider.MediaStore
+import com.gebbers.sonata.domain.model.Song
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MusicScanner @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    fun scanInternalStorage(): List<Song> {
+        val songs = mutableListOf<Song>()
+        val collection = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.DATA,
+            MediaStore.Audio.Media.ALBUM_ID
+        )
+
+        val selection = "${MediaStore.Audio.Media.IS_MUSIC} != 0"
+        val sortOrder = "${MediaStore.Audio.Media.TITLE} ASC"
+
+        context.contentResolver.query(
+            collection,
+            projection,
+            selection,
+            null,
+            sortOrder
+        )?.use { cursor ->
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val titleColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
+            val artistColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
+            val albumColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)
+            val durationColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+            val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)
+            val albumIdColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ID)
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn)
+                val title = cursor.getString(titleColumn) ?: "Unknown"
+                val artist = cursor.getString(artistColumn) ?: "Unknown"
+                val album = cursor.getString(albumColumn) ?: "Unknown"
+                val duration = cursor.getLong(durationColumn)
+                val data = cursor.getString(dataColumn)
+                val albumId = cursor.getLong(albumIdColumn)
+                val contentUri = ContentUris.withAppendedId(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    id
+                ).toString()
+
+                songs.add(
+                    Song(
+                        mediaStoreId = id,
+                        title = title,
+                        artist = artist,
+                        album = album,
+                        duration = duration,
+                        dataPath = data,
+                        uri = contentUri,
+                        albumId = albumId
+                    )
+                )
+            }
+        }
+        return songs
+    }
+}
