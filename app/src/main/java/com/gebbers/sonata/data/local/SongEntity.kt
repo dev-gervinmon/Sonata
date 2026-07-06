fcea8f9362002148dd3bd98f2db79298bf1c7102
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-06 16:37:12 +0800

    feat: create SongEntity Room entity

---
+++ app/src/main/java/com/gebbers/sonata/data/local/SongEntity.kt
@@ -0,0 +1,17 @@
+package com.gebbers.sonata.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "songs")
+data class SongEntity(
+    @PrimaryKey val mediaStoreId: Long,
+    val title: String,
+    val artist: String,
+    val album: String,
+    val duration: Long,
+    val dataPath: String,
+    val uri: String,
+    val albumId: Long,
+    val dateAdded: Long
+)
