9f570de503f0ef851abc6479cd88d1a95f98120d
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-07 16:44:30 +0800

    feat: add playlist entities and DAO to database

--- app/src/main/java/com/gebbers/sonata/data/local/SonataDatabase.kt
+++ app/src/main/java/com/gebbers/sonata/data/local/SonataDatabase.kt
@@ -3,9 +3,18 @@ package com.gebbers.sonata.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [SongEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        SongEntity::class,
+        PlaylistEntity::class,
+        PlaylistSongCrossRef::class
+    ],
+    version = 2,
+    exportSchema = false
+)
 abstract class SonataDatabase : RoomDatabase() {
     abstract fun songDao(): SongDao
+    abstract fun playlistDao(): PlaylistDao
 
     companion object {
         const val DATABASE_NAME = "sonata_db"
