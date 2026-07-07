85d5dcf564f236d42db07a993a091afb04ad04d5
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-07 16:54:51 +0800

    config: increment database version

--- app/src/main/java/com/gebbers/sonata/data/local/SonataDatabase.kt
+++ app/src/main/java/com/gebbers/sonata/data/local/SonataDatabase.kt
@@ -9,7 +9,7 @@ import androidx.room.RoomDatabase
         PlaylistEntity::class,
         PlaylistSongCrossRef::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class SonataDatabase : RoomDatabase() {
