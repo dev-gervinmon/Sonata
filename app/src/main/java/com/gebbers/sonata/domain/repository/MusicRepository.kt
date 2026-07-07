5bb5a1fddc7e0f6258525e93e3636a97b225a4d7
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-07 16:40:29 +0800

    feat: add getSongsByFolder and getAllFolders

--- app/src/main/java/com/gebbers/sonata/domain/repository/MusicRepository.kt
+++ app/src/main/java/com/gebbers/sonata/domain/repository/MusicRepository.kt
@@ -1,10 +1,13 @@
 package com.gebbers.sonata.domain.repository
 
+import com.gebbers.sonata.domain.model.Folder
 import com.gebbers.sonata.domain.model.Song
 import kotlinx.coroutines.flow.Flow
 
 interface MusicRepository {
     fun getAllSongs(): Flow<List<Song>>
+    fun getSongsByFolder(folderPath: String): Flow<List<Song>>
+    fun getAllFolders(): Flow<List<Folder>>
     fun searchSongs(query: String): Flow<List<Song>>
     suspend fun refreshLibrary()
 }
