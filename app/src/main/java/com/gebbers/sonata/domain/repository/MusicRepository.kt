50ed64d8853e2cd85c3ac03f790f5465a50220da
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-07 16:45:19 +0800

    feat: add playlist management methods to MusicRepository

--- app/src/main/java/com/gebbers/sonata/domain/repository/MusicRepository.kt
+++ app/src/main/java/com/gebbers/sonata/domain/repository/MusicRepository.kt
@@ -1,6 +1,7 @@
 package com.gebbers.sonata.domain.repository
 
 import com.gebbers.sonata.domain.model.Folder
+import com.gebbers.sonata.domain.model.Playlist
 import com.gebbers.sonata.domain.model.Song
 import kotlinx.coroutines.flow.Flow
 
@@ -8,6 +9,14 @@ interface MusicRepository {
     fun getAllSongs(): Flow<List<Song>>
     fun getSongsByFolder(folderPath: String): Flow<List<Song>>
     fun getAllFolders(): Flow<List<Folder>>
+    
+    fun getAllPlaylists(): Flow<List<Playlist>>
+    fun getSongsInPlaylist(playlistId: Long): Flow<List<Song>>
+    suspend fun createPlaylist(name: String)
+    suspend fun deletePlaylist(playlist: Playlist)
+    suspend fun addSongToPlaylist(playlistId: Long, mediaStoreId: Long)
+    suspend fun removeSongFromPlaylist(playlistId: Long, mediaStoreId: Long)
+
     fun searchSongs(query: String): Flow<List<Song>>
     suspend fun refreshLibrary()
 }
