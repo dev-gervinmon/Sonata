fdb1f092bca5fd3117c76619d09ba7aac8240e6b
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-08 18:30:44 +0800

    feat: add lyrics field to SongEntity

--- app/src/main/java/com/gebbers/sonata/data/local/SongEntity.kt
+++ app/src/main/java/com/gebbers/sonata/data/local/SongEntity.kt
@@ -16,5 +16,6 @@ data class SongEntity(
     val dateAdded: Long,
     val isFavorite: Boolean = false,
     val playCount: Int = 0,
-    val lastPlayedAt: Long = 0
+    val lastPlayedAt: Long = 0,
+    val lyrics: String? = null
 )
