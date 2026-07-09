c77aa6d24876949cc5d5e44db70f56c8b641608c
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-09 12:15:28 +0800

    ui: remove unused imports from MiniPlayer

--- app/src/main/java/com/gebbers/sonata/ui/playback/MiniPlayer.kt
+++ app/src/main/java/com/gebbers/sonata/ui/playback/MiniPlayer.kt
@@ -14,8 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
