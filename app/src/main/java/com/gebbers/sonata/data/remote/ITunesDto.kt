b5717fddff07eada714279d00cbf47b8ab87adea
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-09 15:05:24 +0800

    feat: add ITunesDto for iTunes search API results

---
+++ app/src/main/java/com/gebbers/sonata/data/remote/ITunesDto.kt
@@ -0,0 +1,16 @@
+package com.gebbers.sonata.data.remote
+
+import com.google.gson.annotations.SerializedName
+
+data class ITunesSearchResponse(
+    @SerializedName("results") val results: List<ITunesResult>
+)
+
+data class ITunesResult(
+    @SerializedName("artworkUrl100") val artworkUrl100: String,
+    @SerializedName("collectionName") val albumName: String,
+    @SerializedName("artistName") val artistName: String
+) {
+    val artworkUrlHighRes: String
+        get() = artworkUrl100.replace("100x100bb.jpg", "600x600bb.jpg")
+}
