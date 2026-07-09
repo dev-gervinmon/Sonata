f0c231a70ae6650ef513dd7bbf74b6049f933c8d
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-09 14:05:13 +0800

    data: increment database version to 8

--- app/src/main/java/com/gebbers/sonata/data/local/SonataDatabase.kt
+++ app/src/main/java/com/gebbers/sonata/data/local/SonataDatabase.kt
@@ -11,7 +11,7 @@ import androidx.room.RoomDatabase
         ExcludedFolderEntity::class,
         ScannedFolderEntity::class
     ],
-    version = 7,
+    version = 8,
     exportSchema = false
 )
 abstract class SonataDatabase : RoomDatabase() {
