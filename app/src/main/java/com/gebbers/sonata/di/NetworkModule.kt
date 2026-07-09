3bf184d48c027fd12e189e2227ba97884a99798b
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-09 15:06:03 +0800

    feat: add NetworkModule for iTunes search service

---
+++ app/src/main/java/com/gebbers/sonata/di/NetworkModule.kt
@@ -0,0 +1,25 @@
+package com.gebbers.sonata.di
+
+import com.gebbers.sonata.data.remote.ITunesSearchService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideITunesSearchService(): ITunesSearchService {
+        return Retrofit.Builder()
+            .baseUrl("https://itunes.apple.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ITunesSearchService::class.java)
+    }
+}
