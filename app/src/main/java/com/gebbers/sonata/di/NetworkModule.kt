8501a6170b13b27e9776265416221b20ccb8aa0a
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-07-11 12:09:51 +0800

    feat: add LyricsService and configure named Retrofit providers in NetworkModule

--- app/src/main/java/com/gebbers/sonata/di/NetworkModule.kt
+++ app/src/main/java/com/gebbers/sonata/di/NetworkModule.kt
@@ -1,12 +1,14 @@
 package com.gebbers.sonata.di
 
 import com.gebbers.sonata.data.remote.ITunesSearchService
+import com.gebbers.sonata.data.remote.LyricsService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -15,11 +17,33 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideITunesSearchService(): ITunesSearchService {
+    @Named("iTunes")
+    fun provideITunesRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://itunes.apple.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(ITunesSearchService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    @Named("lyrics")
+    fun provideLyricsRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.lyrics.ovh/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideITunesSearchService(@Named("iTunes") retrofit: Retrofit): ITunesSearchService {
+        return retrofit.create(ITunesSearchService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLyricsService(@Named("lyrics") retrofit: Retrofit): LyricsService {
+        return retrofit.create(LyricsService::class.java)
     }
 }
